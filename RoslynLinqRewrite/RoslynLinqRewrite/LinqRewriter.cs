45f539b1da8115e1a9d5ae63d8d4867e9cd41f1f
Andrea Martinelli <antiufo@users.noreply.github.com>
2015-08-28 16:56:46 +0200

    moved rules to separate file

--- RoslynLinqRewrite/RoslynLinqRewrite/LinqRewriter.cs
+++ RoslynLinqRewrite/RoslynLinqRewrite/LinqRewriter.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace RoslynLinqRewrite
 {
-    internal class LinqRewriter : CSharpSyntaxRewriter
+    public partial class LinqRewriter : CSharpSyntaxRewriter
     {
         private SemanticModel semantic;
 
@@ -119,235 +119,12 @@ namespace RoslynLinqRewrite
                     var returnType = SyntaxFactory.ParseTypeName(semanticReturnType.ToDisplayString());
 
                     var aggregationMethod = methodNames[0];
-
-                    if (aggregationMethod == WhereMethod || aggregationMethod == SelectMethod)
-                    {
-                        return RewriteAsLoop(
-                            returnType,
-                            Enumerable.Empty<StatementSyntax>(),
-                            Enumerable.Empty<StatementSyntax>(),
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return SyntaxFactory.YieldStatement(SyntaxKind.YieldReturnStatement, SyntaxFactory.IdentifierName(param.Identifier.ValueText));
-                            },
-                            true
-                        );
-                    }
-
-                    if (aggregationMethod == SumIntsMethod)
-                    {
-                        return RewriteAsLoop(
-                            CreatePrimitiveType(SyntaxKind.IntKeyword),
-                            new[] { CreateLocalVariableDeclaration("sum_", SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0))) },
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("sum_")) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-
-                                return SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.AddAssignmentExpression, SyntaxFactory.IdentifierName("sum_"), SyntaxFactory.IdentifierName(param.Identifier.ValueText)));
-                            }
-                        );
-                    }
-                    if (aggregationMethod == AnyWithConditionMethod)
-                    {
-
-                        return RewriteAsLoop(
-                            CreatePrimitiveType(SyntaxKind.BoolKeyword),
-                            Enumerable.Empty<StatementSyntax>(),
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                var lambda = (LambdaExpressionSyntax)inv.ArgumentList.Arguments.First().Expression;
-
-                                return SyntaxFactory.IfStatement(InlineOrCreateMethod(lambda, CreatePrimitiveType(SyntaxKind.BoolKeyword), arguments, param),
-                                 SyntaxFactory.ReturnStatement(SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression)
-                                 ));
-                            }
-                        );
-                    }
-                    if (aggregationMethod == AnyMethod)
-                    {
-
-                        return RewriteAsLoop(
-                            CreatePrimitiveType(SyntaxKind.BoolKeyword),
-                            Enumerable.Empty<StatementSyntax>(),
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return SyntaxFactory.ReturnStatement(SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression));
-                            }
-                        );
-                    }
-
-
-
-                    if (aggregationMethod == FirstMethod)
-                    {
-                        return RewriteAsLoop(
-                            SyntaxFactory.ParseTypeName(semantic.GetTypeInfo(node).ConvertedType.ToDisplayString()),
-                            Enumerable.Empty<StatementSyntax>(),
-                            new[] { CreateThrowException("System.InvalidOperationException", "The sequence did not contain any elements.") },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName(param.Identifier.ValueText));
-                            }
-                        );
-                    }
-
-
-
-                    if (aggregationMethod == FirstOrDefaultMethod)
-                    {
-                        return RewriteAsLoop(
-                            returnType,
-                            Enumerable.Empty<StatementSyntax>(),
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.DefaultExpression(returnType)) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName(param.Identifier.ValueText));
-                            }
-                        );
-                    }
-
-                    if (aggregationMethod == LastOrDefaultMethod)
-                    {
-                        return RewriteAsLoop(
-                            returnType,
-                            new[] { CreateLocalVariableDeclaration("_last", SyntaxFactory.DefaultExpression(returnType)) },
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("_last")) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, SyntaxFactory.IdentifierName("_last"), SyntaxFactory.IdentifierName(param.Identifier.ValueText)));
-                            }
-                        );
-                    }
-
-
-                    if (aggregationMethod == ToListMethod)
-                    {
-                        var listIdentifier = SyntaxFactory.IdentifierName("_list");
-                        return RewriteAsLoop(
-                            returnType,
-                            new[] { CreateLocalVariableDeclaration("_list", SyntaxFactory.ObjectCreationExpression(returnType, CreateArguments(Enumerable.Empty<ArgumentSyntax>()), null)) },
-                            new[] { SyntaxFactory.ReturnStatement(listIdentifier) },
-                            collection,
-                            chain,
-                            (inv, arguments, param) =>
-                            {
-                                return CreateStatement(SyntaxFactory.InvocationExpression(SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, listIdentifier, SyntaxFactory.IdentifierName("Add")), CreateArguments(new[] { SyntaxFactory.IdentifierName(param.Identifier.ValueText) })));
-                            }
-                        );
-                    }
-#if false
-
-
-                    if (IsMethodSequence(SumWithSelectorMethod, WhereMethod))
-                    {
-                        string itemArg = null;
-                        return RewriteAsLoop(
-                            CreatePrimitiveType(SyntaxKind.IntKeyword),
-                            new[] { CreateLocalVariableDeclaration("sum_", SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0))) },
-                            arguments =>
-                            {
-                                return SyntaxFactory.IfStatement(
-                                InlineOrCreateMethod((CSharpSyntaxNode)Visit(lambda.Body), arguments, CreateParameter(arg.Identifier, itemType), out itemArg),
-                                SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.AddAssignmentExpression, SyntaxFactory.IdentifierName("sum_"),
-                                     InlineOrCreateMethod((CSharpSyntaxNode)Visit(lambda.Body), arguments, CreateParameter(arg.Identifier, itemType), out itemArg)))
-                                     );
-
-                            },
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("sum_")) },
-                            () => itemArg,
-                            collection
-                        );
-
-                    }
-
- 
-
-                    if (GetMethodFullName(node) == WhereMethod)
-                    {
-
-                        string itemArg = null;
-                        return RewriteAsLoop(
-                            SyntaxFactory.ParseTypeName("System.Collections.Generic.IEnumerable<" + itemType.ToDisplayString() + ">"),
-                            Enumerable.Empty<StatementSyntax>(),
-                            arguments =>
-                            {
-                                return SyntaxFactory.IfStatement(
-                                 InlineOrCreateMethod((CSharpSyntaxNode)Visit(lambda.Body), arguments, CreateParameter(arg.Identifier, itemType), out itemArg),
-                                 SyntaxFactory.YieldStatement(SyntaxKind.YieldReturnStatement, SyntaxFactory.IdentifierName(arg.Identifier))
-                             );
-                            },
-                            Enumerable.Empty<StatementSyntax>(),
-                            () => itemArg,
-                            collection
-                        );
-                    }
-
-                    if (GetMethodFullName(node) == SumWithSelectorMethod)
-                    {
-
-                        string itemArg = null;
-
-
-
-
-
-                        return RewriteAsLoop(
-                           CreatePrimitiveType(SyntaxKind.IntKeyword),
-                           new[] { CreateLocalVariableDeclaration("sum_", SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0))) },
-                           arguments =>
-                           {
-                               return SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.AddAssignmentExpression, SyntaxFactory.IdentifierName("sum_"),
-                                    InlineOrCreateMethod((CSharpSyntaxNode)Visit(lambda.Body), arguments, CreateParameter(arg.Identifier, itemType), out itemArg)));
-
-                           },
-                           new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("sum_")) },
-                           () => itemArg,
-                           collection
-                       );
-
-
-
-                    }
-
-
-                    if (GetMethodFullName(node) == SumIntsMethod)
-                    {
-                        string itemArg = null;
-                        return RewriteAsLoop(
-                            CreatePrimitiveType(SyntaxKind.IntKeyword),
-                            new[] { CreateLocalVariableDeclaration("sum_", SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0))) },
-                            arguments =>
-                            {
-                                return SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.AddAssignmentExpression, SyntaxFactory.IdentifierName("sum_"),
-                                     InlineOrCreateMethod(SyntaxFactory.IdentifierName(ItemName), arguments, CreateParameter(ItemName, itemType), out itemArg)));
-
-                            },
-                            new[] { SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("sum_")) },
-                            () => itemArg,
-                            collection
-                        );
-                    }
-#endif
+                    return TryRewrite(aggregationMethod, collection, returnType, chain, node);
                 }
             }
             return null;
         }
+
 
         private StatementSyntax CreateStatement(ExpressionSyntax expression)
         {
@@ -374,53 +151,7 @@ namespace RoslynLinqRewrite
                 (lambda as AnonymousMethodExpressionSyntax)?.ParameterList.Parameters[index];
         }
 
-        private StatementSyntax CreateProcessingStep(List<InvocationExpressionSyntax> chain, int chainIndex, TypeSyntax itemType, string itemName, ArgumentListSyntax arguments, bool noAggregation)
-        {
-
-            if (chainIndex == 0 && !noAggregation || chainIndex == -1)
-            {
-                return currentAggregation(chain[0], arguments, CreateParameter(itemName, itemType));
-            }
-
-            var invocationExpressionSyntax = chain[chainIndex];
-
-
-            var method = GetMethodFullName(invocationExpressionSyntax);
-
-
-
-            if (method == WhereMethod)
-            {
-                var lambda = (AnonymousFunctionExpressionSyntax)invocationExpressionSyntax.ArgumentList.Arguments[0].Expression;
-
-                var check = InlineOrCreateMethod(lambda, CreatePrimitiveType(SyntaxKind.BoolKeyword), arguments, CreateParameter(itemName, itemType));
-                var next = CreateProcessingStep(chain, chainIndex - 1, itemType, itemName, arguments, noAggregation);
-                return SyntaxFactory.IfStatement(check, next is BlockSyntax ? next : SyntaxFactory.Block(next));
-            }
-
-
-            if (method == SelectMethod)
-            {
-                var lambda = (LambdaExpressionSyntax)invocationExpressionSyntax.ArgumentList.Arguments[0].Expression;
-
-                var newname = "gattone" + ++lastId;
-                var lambdaType = (INamedTypeSymbol)semantic.GetTypeInfo(lambda).ConvertedType;
-                var lambdaBodyType = lambdaType.TypeArguments.Last();
-                var newtype = IsAnonymousType(lambdaBodyType) ? null : SyntaxFactory.ParseTypeName(lambdaBodyType.ToDisplayString());
-
-
-                var local = CreateLocalVariableDeclaration(newname, InlineOrCreateMethod(lambda, newtype, arguments, CreateParameter(itemName, itemType)));
-
-
-                var next = CreateProcessingStep(chain, chainIndex - 1, newtype, newname, arguments, noAggregation);
-                var nexts = next is BlockSyntax ? ((BlockSyntax)next).Statements : (IEnumerable<StatementSyntax>)new[] { next };
-                return SyntaxFactory.Block(new[] { local }.Concat(nexts));
-            }
-
-
-
-            throw new NotImplementedException();
-        }
+ 
 
         private AnonymousFunctionExpressionSyntax RenameSymbol(AnonymousFunctionExpressionSyntax container, int argIndex, string newname)
         {
@@ -454,17 +185,7 @@ namespace RoslynLinqRewrite
             //return annotated.WithoutAnnotations();
         }
 
-        readonly static string ToListMethod = "System.Collections.Generic.IEnumerable<TSource>.ToList<TSource>()";
-        readonly static string FirstMethod = "System.Collections.Generic.IEnumerable<TSource>.First<TSource>()";
-        readonly static string FirstOrDefaultMethod = "System.Collections.Generic.IEnumerable<TSource>.FirstOrDefault<TSource>()";
-        readonly static string LastOrDefaultMethod = "System.Collections.Generic.IEnumerable<TSource>.LastOrDefault<TSource>()";
-        readonly static string AnyMethod = "System.Collections.Generic.IEnumerable<TSource>.Any<TSource>()";
-        readonly static string AnyWithConditionMethod = "System.Collections.Generic.IEnumerable<TSource>.Any<TSource>(System.Func<TSource, bool>)";
-        readonly static string SumWithSelectorMethod = "System.Collections.Generic.IEnumerable<TSource>.Sum<TSource>(System.Func<TSource, int>)";
-        readonly static string SumIntsMethod = "System.Collections.Generic.IEnumerable<int>.Sum()";
-        readonly static string WhereMethod = "System.Collections.Generic.IEnumerable<TSource>.Where<TSource>(System.Func<TSource, bool>)";
-        readonly static string SelectMethod = "System.Collections.Generic.IEnumerable<TSource>.Select<TSource, TResult>(System.Func<TSource, TResult>)";
-
+        
 
         ITypeSymbol GetSymbolType(VariableCapture x)
         {
