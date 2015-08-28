ad5530af4c6a2cb902ed8f355991dab251088be6
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-08-28 23:40:58 +0200

    forgotten file

---
+++ RoslynLinqRewrite/RoslynLinqRewrite/LinqStep.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynLinqRewrite
+{
+    class LinqStep
+    {
+        public LinqStep(string methodName, IReadOnlyList<ExpressionSyntax> arguments)
+        {
+            this.MethodName = methodName;
+            this.Arguments = arguments;
+        }
+        public string MethodName { get; }
+        public IReadOnlyList<ExpressionSyntax> Arguments { get; }
+    }
+}
