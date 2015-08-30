18ceb3a1f0f6442c2de4c5c1ad61d1d681c8efe3
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-08-30 13:15:12 +0200

    forgotten file

---
+++ RoslynLinqRewrite/RoslynLinqRewrite/CanRewrapForeachVisitor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynLinqRewrite
+{
+    class CanRewrapForeachVisitor : CSharpSyntaxWalker
+    {
+        internal bool Fail;
+        public override void VisitAwaitExpression(AwaitExpressionSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitBreakStatement(BreakStatementSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitContinueStatement(ContinueStatementSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitReturnStatement(ReturnStatementSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitGotoStatement(GotoStatementSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitYieldStatement(YieldStatementSyntax node)
+        {
+            Fail = true;
+        }
+
+        public override void VisitParenthesizedLambdaExpression(ParenthesizedLambdaExpressionSyntax node)
+        {
+        }
+
+        public override void VisitSimpleLambdaExpression(SimpleLambdaExpressionSyntax node)
+        {
+        }
+
+        public override void VisitAnonymousMethodExpression(AnonymousMethodExpressionSyntax node)
+        {
+        }
+    }
+}
