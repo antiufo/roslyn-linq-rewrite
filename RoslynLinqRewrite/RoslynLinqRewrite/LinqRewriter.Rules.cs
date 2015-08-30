6f3ce96ffdd4fb3ad08a595ce91390405d362c78
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-08-30 09:55:18 +0200

    support for Min and Max

--- RoslynLinqRewrite/RoslynLinqRewrite/LinqRewriter.Rules.cs
+++ RoslynLinqRewrite/RoslynLinqRewrite/LinqRewriter.Rules.cs
@@ -52,8 +52,41 @@ namespace RoslynLinqRewrite
                 );
             }
 
+            if (aggregationMethod.Contains(".Max") || aggregationMethod.Contains(".Min"))
+            {
+                var minmax = aggregationMethod.Contains(".Max") ? "max_" : "min_";
+                var elementType = ((returnType as NullableTypeSyntax)?.ElementType ?? returnType);
+                return RewriteAsLoop(
+                    returnType,
+                    new[] {
+                        CreateLocalVariableDeclaration("found_", SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)),
+                        CreateLocalVariableDeclaration(minmax, SyntaxFactory.CastExpression(elementType, SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0))))
+                    },
+                    new[] {
+                        SyntaxFactory.Block(
+                        SyntaxFactory.IfStatement(SyntaxFactory.PrefixUnaryExpression(SyntaxKind.LogicalNotExpression, SyntaxFactory.IdentifierName("found_")),
+                            returnType == elementType ? (StatementSyntax)CreateThrowException("System.InvalidOperationException", "The sequence did not contain any elements.") :
+                            SyntaxFactory.ReturnStatement(SyntaxFactory.LiteralExpression(SyntaxKind.NullLiteralExpression))
+                        ),
+                         SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName(minmax))
+                        )
+                    },
+                    collection,
+                    MaybeAddSelect(chain, node.ArgumentList.Arguments.Count != 0),
+                    (inv, arguments, param) =>
+                    {
+                        var identifierNameSyntax = SyntaxFactory.IdentifierName(param.Identifier.ValueText);
+                        return IfNullableIsNotNull(elementType != returnType, identifierNameSyntax, x =>
+                        {
+                            var assignmentExpressionSyntax = SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, SyntaxFactory.IdentifierName(minmax), x);
+                            return SyntaxFactory.IfStatement( SyntaxFactory.IdentifierName("found_"),
+                               SyntaxFactory.Block( SyntaxFactory.IfStatement(SyntaxFactory.BinaryExpression(aggregationMethod.Contains(".Max") ? SyntaxKind.GreaterThanExpression : SyntaxKind.LessThanExpression, x, SyntaxFactory.IdentifierName(minmax)), SyntaxFactory.ExpressionStatement(assignmentExpressionSyntax))), 
+                               SyntaxFactory.ElseClause(SyntaxFactory.Block(SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, SyntaxFactory.IdentifierName("found_"), SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression))) ,SyntaxFactory.ExpressionStatement(assignmentExpressionSyntax))));
+                        });
+                    });
+            }
 
-
+            
             if (aggregationMethod.Contains(".Average"))
             {
                 var elementType = ((returnType as NullableTypeSyntax)?.ElementType ?? returnType);
