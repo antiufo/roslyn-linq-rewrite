60e2780085476f9eb91fb3aa16d4216f65a6fa21
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-09-09 11:30:48 +0200

    compatibility

---
+++ RoslynLinqRewrite/RoslynLinqRewrite/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dnx.Runtime
+{
+    internal static class Logger
+    {
+        internal static void TraceError(string v)
+        {
+        }
+
+        internal static void TraceInformation(string v)
+        {
+        }
+    }
+}
