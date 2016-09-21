9563a18e59f06bfbf4ffcb3940f62c3a11bfb2e4
Andrea Martinelli <andmartinelli@gmail.com>
2016-09-21 15:29:08 +0200

    Fix missing assembly exception

---
+++ RoslynLinqRewrite/Initialize.cmd
@@ -0,0 +1,14 @@
+@echo off
+cd /d %~dp0
+
+dotnet restore dotnet-compile-csc-linq-rewrite
+if not exist RoslynLinqRewrite\bin\Debug mkdir RoslynLinqRewrite\bin\Debug
+if not exist RoslynLinqRewrite\bin\Release mkdir RoslynLinqRewrite\bin\Release
+
+
+copy /y %userprofile%\.nuget\packages\System.Linq\4.1.0\lib\net463\*.dll RoslynLinqRewrite\bin\Debug 
+copy /y %userprofile%\.nuget\packages\System.Linq\4.1.0\lib\net463\*.dll RoslynLinqRewrite\bin\Release
+
+copy /y %userprofile%\.nuget\packages\System.Runtime.Loader\4.0.0\lib\netstandard1.5\*.dll RoslynLinqRewrite\bin\Debug 
+copy /y %userprofile%\.nuget\packages\System.Runtime.Loader\4.0.0\lib\netstandard1.5\*.dll RoslynLinqRewrite\bin\Release
+
