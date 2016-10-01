c43ddd2ebd38ae21318d158d6579471b5a18bb36
Andrea Martinelli <andmartinelli@gmail.com>
2016-10-01 14:14:12 +0200

    Remove old fields

--- RoslynLinqRewrite/RoslynLinqRewrite/Program.cs
+++ RoslynLinqRewrite/RoslynLinqRewrite/Program.cs
@@ -24,8 +24,6 @@ namespace Shaman.Roslyn.LinqRewrite
 {
     class Program
     {
-        private static bool NoRewrite;
-        private static bool WriteFiles;
         
 #if false
         private static IServiceProvider _hostServices;
